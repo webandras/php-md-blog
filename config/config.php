06275f3bbfcfb81d2ca002016c01082f8fecc84b
Andras.Gulacsi <Andras.Gulacsi@medicover.hu>
2026-01-09 15:28:02 +0100

    Fix urls

--- config/config.php
+++ config/config.php
@@ -3,7 +3,7 @@
 // The main config file
 // $env comes from the command line option (see the readme)
 if ($env === 'dev') {
-    define('BASE_URL', 'http://localhost/phpmd_blog/public/');
+    define('BASE_URL', 'http://localhost/php-md-blog/public/');
 } else {
     define('BASE_URL', 'https://phpmd.webandras.hu/');
 }
@@ -14,7 +14,6 @@ define('DEFAULT_LANGUAGE', 'en-gb');
 define('LANGUAGES', array(
     'en-gb' => 'English',
     'hu-hu' => 'Magyar',
-    'lt-lt' => 'Lietuvių',
 ));
 
 define('POST_LIMIT', 10);
