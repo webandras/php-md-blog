52245cb49b7725147130a7130ad6aa5fe5471dd5
Andras.Gulacsi <Andras.Gulacsi@medicover.hu>
2026-01-09 15:27:30 +0100

    Code formatting, remove unnecessary codes

--- engine/PHP_MD.php
+++ engine/PHP_MD.php
@@ -19,34 +19,34 @@ class PHP_MD extends PHP_MD_Base
     }
 
 
-	/**
-	 * @param  string  $language
-	 * @param  array   $data
-	 * @param  bool    $force_build
-	 *
-	 * @return array
-	 */
+    /**
+     * @param  string  $language
+     * @param  array  $data
+     * @param  bool  $force_build
+     *
+     * @return array
+     */
     public function generate_posts(string $language = DEFAULT_LANGUAGE, array $data = [], bool $force_build = false): array
     {
-	    $this->posts           = [];
-	    $source_directory      = $this->root_dir . '/posts/' . $this->get_language_segment($language);
-	    $destination_directory = $this->root_dir . '/public/posts/' . $this->get_language_segment($language);
-	    $files                 = glob($source_directory.'/*.md');
-	    $lc_time               = $this->get_lc_time($language);
-	    $this->force_build     = $force_build;
-	    $last_build_date       = $this->get_build_time();
+        $this->posts = [];
+        $source_directory = $this->root_dir.'/posts/'.$this->get_language_segment($language);
+        $destination_directory = $this->root_dir.'/public/posts/'.$this->get_language_segment($language);
+        $files = glob($source_directory.'/*.md');
+        $lc_time = $this->get_lc_time($language);
+        $this->force_build = $force_build;
+        $last_build_date = $this->get_build_time();
 
         // Read all markdown files and convert them to html
         foreach ($files as $filepath) {
             $markdown = file_get_contents($filepath);
-			$file_modification_date = filemtime($filepath);
+            $file_modification_date = filemtime($filepath);
 
-			// Do not run markdown conversion if the file hasn't been modified since the last build, unless the force option is used
-			if ($this->force_build === false && $last_build_date !== 0 && $file_modification_date < $last_build_date) {
-				continue;
-			}
+            // Do not run markdown conversion if the file hasn't been modified since the last build, unless the force option is used
+            if ($this->force_build === false && $last_build_date !== 0 && $file_modification_date < $last_build_date) {
+                continue;
+            }
 
-			echo 'Post re-generated' . PHP_EOL;
+            echo 'Post re-generated'.PHP_EOL;
 
             try {
                 $result = $this->converter->convert($markdown);
@@ -57,17 +57,17 @@ class PHP_MD extends PHP_MD_Base
 
             if ($result instanceof RenderedContentWithFrontMatter) {
                 $frontMatter = $this->transform_front_matter(
-                    $this->get_post_front_matter($result),
-                    $filepath,
-                    $language,
-                    $data['timezone'],
-                    $lc_time
+                    front_matter: $this->get_post_front_matter($result),
+                    filepath: $filepath,
+                    language: $language,
+                    timezone: $data['timezone'],
+                    lc_time: $lc_time
                 );
 
-                $this->posts[]          = $frontMatter;
-                $data['frontmatter']    = $frontMatter;
-                $data['content']        = $this->get_post_content($result);
-                $data['template_name']  = 'single';
+                $this->posts[] = $frontMatter;
+                $data['frontmatter'] = $frontMatter;
+                $data['content'] = $this->get_post_content($result);
+                $data['template_name'] = 'single';
 
                 $content = $this->render_view_and_return($data);
 
@@ -89,9 +89,9 @@ class PHP_MD extends PHP_MD_Base
      */
     public function generate_index_page(string $language = DEFAULT_LANGUAGE, array $data = []): void
     {
-        $destination_directory  = $this->root_dir.'/public/'.$this->get_language_segment($language);
-		$data['posts']          = array_slice($this->posts, 0, POST_LIMIT);
-        $data['template_name']  = 'index';
+        $destination_directory = $this->root_dir.'/public/'.$this->get_language_segment($language);
+        $data['posts'] = array_slice($this->posts, 0, POST_LIMIT);
+        $data['template_name'] = 'index';
 
         $content = $this->render_view_and_return($data);
 
@@ -107,17 +107,17 @@ class PHP_MD extends PHP_MD_Base
      */
     public function generate_archive_page(string $language = DEFAULT_LANGUAGE, array $data = []): void
     {
-        $destination_directory  = $this->root_dir . '/public/' . $this->get_language_segment($language);
-        $data['posts']          = $this->posts;
-        $data['template_name']  = 'archive';
-        $data['lc_time']        = $this->get_lc_time($language);
+        $destination_directory = $this->root_dir.'/public/'.$this->get_language_segment($language);
+        $data['posts'] = $this->posts;
+        $data['template_name'] = 'archive';
+        $data['lc_time'] = $this->get_lc_time($language);
 
         try {
             /* Create post list grouped by month/year */
             $year_month_groups = [];
             foreach ($this->posts as $post) {
-                $post_date  = new \DateTime($post['date_original'], new \DateTimeZone($data['timezone']));
-                $formatter  = $this->get_local_datetime_formatter($data['lc_time'], $data['timezone'], $data['pattern']);
+                $post_date = new \DateTime($post['date_original'], new \DateTimeZone($data['timezone']));
+                $formatter = $this->get_local_datetime_formatter($data['lc_time'], $data['timezone'], $data['pattern']);
 
                 $year_month_groups[$post_date->format('M Y')] = $formatter->format($post_date);
             }
@@ -141,12 +141,11 @@ class PHP_MD extends PHP_MD_Base
      */
     public function generate_404_page(string $language = DEFAULT_LANGUAGE, array $data = []): void
     {
-        $destination_directory  = $this->root_dir . '/public/' . $this->get_language_segment($language);
-        $data['template_name']  = '404';
+        $destination_directory = $this->root_dir.'/public/'.$this->get_language_segment($language);
+        $data['template_name'] = '404';
 
         $content = $this->render_view_and_return($data);
 
         $this->save_html($destination_directory, $content, '', '404');
     }
-
 }
--- extension/PHP_MD_Trait.php
+++ extension/PHP_MD_Trait.php
@@ -1,23 +1,13 @@
 <?php
 
 /**
- * This trait is intended for the extension of the site builder
- * Put your custom code here. This file is not going to be overwritten by future releases.
+ * This trait is intended for the extension of the site builder engine.
+ * Put your custom code here.
  */
 trait PHP_MD_Trait
 {
-    // Implement it
     public function generate_about_page(): void
     {
         echo 'Generate about page...';
     }
-
-    // Implement it
-    public function generate_contact_page(): void
-    {
-        echo 'Generate contact page...';
-    }
-
-    // and so on...
-
 }
